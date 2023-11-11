f7b10692b8104ca15a229d9acd520651f77bd494
Puneet1420 <puneetbajpai1420@gmail.com>
2023-11-11 23:05:33 +0530

    configure data base conncetion , created error class and success class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from 'dotenv'
 import connect_DB from "./db/index.js";
+import app from "./app.js"
 
 dotenv.config({
     path: './src/.env'
 })
 
-connect_DB();
+connect_DB()
+.then(() => {
+    app.listen(process.env.port || 8001, () => {
+        console.log(`Server is running at Port ${process.env.port}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB DATA CONNECTION FAILED !!!" , err);
+})
 
 
 
